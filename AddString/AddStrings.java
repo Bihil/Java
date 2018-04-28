3d032fae155729b32ca7cbf8a332ebfcdb093700
Bihil <bihilkon@gmail.com>
2018-04-28 16:35:38 +0200

    add string javaIII

--- AddString/AddStrings.java
+++ AddString/AddStrings.java
@@ -31,18 +31,4 @@ public class AddStrings {
 		return getTotal();
 	}
 
-
-	public static void main(String[] args) {
-
-		if (args.length == 1 || args.length > 2)
-		{
-			System.err.println("Useage: AddString integer1 integer2");
-			return;
-		}
-		if(args.length == 0) {
-			System.out.println("There were no commandline arguments passed!");
-		} else {
-			System.out.println(new AddStrings().sum(args[0], args[1]));
-		}
-	}
 }

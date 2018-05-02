ed0173958c9531457c5f5feada4a10e738e99680
Bihil <bihilkon@gmail.com>
2018-05-02 16:50:41 +0200

    Removed methods from AddNUmbers

--- AddString/AddNumbers.java
+++ AddString/AddNumbers.java
@@ -9,27 +9,7 @@ public class AddNumbers implements Add {
 	@Override
 	public int sum(){		
 		return this.var1 +this.var2;
-	}
-
-	public boolean containsOnlyDigits(final String value) {
-		for (int i = 0; i < value.length(); i++) {
-			if(!Character.isDigit(value.charAt(i))) {
-				return false;
-			}
-		}
-		return true;
-	}
-	public void validate(String variable) {
-		if(variable== null) {
-			throw new NullPointerException();
-		}
-		if(variable.length() == 0) {
-			throw new IllegalArgumentException("[ " + variable + " ] is not a positive integer.");
-		}
-		if(!containsOnlyDigits(variable)) {
-			throw new IllegalArgumentException("[ " + variable + " ] is not a positive integer.");
-		}
-	}
+	}	
 
 
 }
